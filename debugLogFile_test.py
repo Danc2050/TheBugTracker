95df9dabab04503f5823bc9e0b5847dd64ce4f78
prikim <prikim@pdx.edu>
2020-05-22 01:52:20 -0700

    Created test for debuglogfile - changed config params of generated by commenting out the removal of the config file in read_config_test that Ramon made. tests were conducted by changing the parameters of the config file and checking each iteration to match

---
+++ debugLogFile_test.py
@@ -0,0 +1,20 @@
+from debugLogFile import *
+import os
+import io
+import sys
+
+message1 = "\n[This is a placeholder for when a new file is created for the debug log file]"
+message2 = "\n[append this message to existing file]"
+message3 = "\n[This message is to overwrite the existing file]"
+
+
+class testdebugLogFile():
+    def test(self):
+        test = debugLogFile()
+        test.writeToFile(message1)
+
+
+if __name__ == '__main__':
+    test_run = testdebugLogFile()
+    test_run.test()
+
