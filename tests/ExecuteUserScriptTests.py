66098e862505b94203e3e0a4125a48b3602f07bc
Dana <dhk2@pdx.edu>
2020-05-30 10:44:01 -0700

    Rebased from master

--- tests/ExecuteUserScriptTests.py
+++ tests/ExecuteUserScriptTests.py
@@ -1,11 +1,13 @@
 import subprocess
 
+
+# Test script broken with Error : src/ExecuteUserScript.py: No such file or directory
 class testScript():
     def __init__(self):
         print("testing")
 
     def testWorkingScript(self):
-        check = subprocess.check_output('executeUserScript.py -S workingTestScript.py', shell=True, text=True)
+        check = subprocess.check_output('src/ExecuteUserScript.py -S tests/WorkingTestScript.py', shell=True, text=True)
         test = check.find('Traceback')
         if test == -1:
             print("Executed workingUserScript successfully. TEST PASSED")
@@ -13,7 +15,7 @@ class testScript():
             print("workingUserScript did not execute. TEST FAILED.")
 
     def testBrokenScript(self):
-        check = subprocess.check_output('executeUserScript.py -S brokenTestScript.py', shell=True, text=True)
+        check = subprocess.check_output('src/ExecuteUserScript.py -S tests/BrokenTestScript.py', shell=True, text=True)
         test = check.find('Traceback')
         if test != -1:
             print("Executed brokenTestScript and Traceback was found. TEST PASSED")
@@ -23,16 +25,18 @@ class testScript():
     def testCommandLine(self):
         try:
             print("Test executing workingUserScript without -S, should fail.")
-            subprocess.check_output('executeUserScript.py workingTestScript.py', shell=True, text=True, stderr=True)
+            subprocess.check_output('src/ExecuteUserScript.py tests/WorkingTestScript.py', shell=True, text=True,
+                                    stderr=True)
         except:
             print("test did fail without -S. TEST PASSED")
             return
-        print("Test passed without -S in command line argguements. TEST FAILED")
+        print("Test passed without -S in command line arguments. TEST FAILED")
 
     def run(self):
         self.testWorkingScript()
         self.testBrokenScript()
         self.testCommandLine()
+
 
 if __name__ == '__main__':
     test_run = testScript()
