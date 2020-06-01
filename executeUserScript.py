d9c28cc53703b62af68c5846a59aec82f8ea2db4
Mahmoud Al Robiai <mahmoudalrobiai@MacBook-Pro.local>
2020-05-31 23:16:07 -0700

    modify executeUserScript.py

--- executeUserScript.py
+++ executeUserScript.py
@@ -14,6 +14,9 @@ class executeUserScript(object):
             return exec(open(scriptName).read())
         except FileNotFoundError:
             print(f'{scriptName} script is not found!')
+        except ModuleNotFoundError as e:
+            print(f'{e}, module is not found!')
+            #we can blacklist the script with missing module and notify user.
         except:
             print(f'{scriptName} did not exit gracefully, Submit a Bug!"')
             traceback.print_exc(file=sys.stdout)
