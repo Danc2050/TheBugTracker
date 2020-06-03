5170e11584a8430c0d4ab4cf1541d0f3b24c336f
prikim <prikim@pdx.edu>
2020-06-02 17:50:38 -0700

    pushing changes before rebase

--- debugLogFile_test.py
+++ debugLogFile_test.py
@@ -11,7 +11,7 @@ message3 = "\n[This message is to overwrite the existing file]"
 class testdebugLogFile():
     def test(self):
         test = debugLogFile()
-        test.writeToFile(message1)
+        test.writeToFile(message2)
 
 
 if __name__ == '__main__':
