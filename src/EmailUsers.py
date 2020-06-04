959136471f2602db6e2c955aaa9301e2d725c2e1
prikim <prikim@pdx.edu>
2020-06-03 21:55:43 -0700

    cleaned code, added class and constructor. created empty test file for QA in test folder.

--- src/EmailUsers.py
+++ src/EmailUsers.py
@@ -1,28 +1,34 @@
 import yagmail
 
-# yagmail.SMTP() without parameters will attempt to locate a .yagmail file in the home directory
-# that contains the gmail username. Should be used in the future to avoid having
-# credentials placed in code
 
-yag = yagmail.SMTP('bugtrackerp@gmail.com', 'D@nkC0d3rs')
-# yag = yagmail.SMTP()
-# another workaround is to call yagmail.register('gmail', 'password') once and have it registered
-# via the keyring library
+class EmailUsers:
+    def __init__(self):
+        # yagmail.SMTP() without parameters will attempt to locate a .yagmail file in the home directory
+        # that contains the gmail username. Should be used in the future to avoid having
+        # credentials placed in code
+        self.yag = yagmail.SMTP()
+        # yag = yagmail.SMTP()
+        # another workaround is to call yagmail.register('gmail', 'password') once and have it registered
+        # via the keyring library
 
+    # sends email to given user 'to_send'
+    def send_email(self,
+                   body,
+                   subject,
+                   to_send):
+        try:
+            self.yag.send(to_send, subject, body)
+            print("Email sent to", to_send)
+        except:
+            print("Something went wrong")
 
-# sends email to given user 'to_send'
-def send_email(body,
-               subject,
-               to_send):
-    yag.send(to_send, subject, body)
-    print("Email sent to", to_send)
-
-
-# sends email to self
-def email_self(body,
-               subject):
-    yag.send(subject, body)
-    # yag.send with no specified user will default to sending the email to yourself (registered user)
-
-
-send_email("default bug report message", "log 0-0-0", "prikim@pdx.edu")
+    # sends email to self
+    def email_self(self,
+                   body,
+                   subject):
+        try:
+            self.yag.send(contents=[body], subject=[subject])
+            # yag.send with no specified user will default to sending the email to yourself (registered user)
+            print("email sent to self")
+        except:
+            print("Something went wrong")
