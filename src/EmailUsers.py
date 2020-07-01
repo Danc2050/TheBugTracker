5f39642c474087c163ea2a74a54a044c2735c2d6
prikim <prikim@pdx.edu>
2020-07-01 15:06:54 -0700

    added environment variables to grab credentials for email

--- src/EmailUsers.py
+++ src/EmailUsers.py
@@ -1,13 +1,13 @@
 import yagmail
+from decouple import config
 
 
 class EmailUsers:
     def __init__(self):
-        # yagmail.SMTP() without parameters will attempt to locate a .yagmail file in the home directory
-        # that contains the gmail username. Should be used in the future to avoid having
-        # credentials placed in code
+        sender = config('USER')
+        key = config('KEY')
+        self.yag = yagmail.register(sender, key)
         self.yag = yagmail.SMTP()
-        # yag = yagmail.SMTP()
         # another workaround is to call yagmail.register('gmail', 'password') once and have it registered
         # via the keyring library
 
@@ -32,3 +32,4 @@ class EmailUsers:
             # yag.send with no specified user will default to sending the email to yourself (registered user)
         except TypeError:
             print("Type mismatch. Contents may be a string, dictionary, file, or HTML")
+
