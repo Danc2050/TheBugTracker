ac88e2cfc0547db9bd097f442d6e2cba018a0435
prikim <prikim@pdx.edu>
2020-07-01 18:55:19 -0700

    cleaned up code

--- src/EmailUsers.py
+++ src/EmailUsers.py
@@ -4,12 +4,10 @@ from decouple import config
 
 class EmailUsers:
     def __init__(self):
-        sender = config('USER')
-        key = config('KEY')
-        self.yag = yagmail.register(sender, key)
-        self.yag = yagmail.SMTP()
-        # another workaround is to call yagmail.register('gmail', 'password') once and have it registered
-        # via the keyring library
+        username = config('USER')
+        password = config('KEY')
+        self.yag = yagmail.register(username, password)
+        self.yag = yagmail.SMTP(username)
 
     # sends email to given user 'to_send'
     def send_email(self,
@@ -29,7 +27,7 @@ class EmailUsers:
                    subject):
         try:
             self.yag.send(contents=[body], subject=[subject])
-            # yag.send with no specified user will default to sending the email to yourself (registered user)
         except TypeError:
             print("Type mismatch. Contents may be a string, dictionary, file, or HTML")
+
 
