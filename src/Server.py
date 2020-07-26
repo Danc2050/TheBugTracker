77f60d6e9ef13128598ca9e6b1cc938691d37862
Cam28 <cam28@pdx.edu>
2020-07-25 20:25:04 -0700

    Updated to host to work on windows as well

--- src/Server.py
+++ src/Server.py
@@ -15,3 +15,4 @@ def issueBug():
 if __name__ == '__main__':
     readConfig.readConfig().createDefault()
     app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='127.0.0.1', debug=True, port=80)
